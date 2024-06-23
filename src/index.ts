1413b60f201a12800cc24949cdb9d4382fee2854
vineeth-0509 <vineeththungani@gmail.com>
2024-06-23 20:18:10 +0530

    created a cloudflare worker that is running,which able to connect database through pooled connection

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,42 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+
+import {PrismaClient} from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env{
+	DATABASE_URL: string
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		async fetch(
+			request: Request,
+			env: Env,
+			ctx: ExecutionContext
+		): Promise<Response>{
+		const prisma= new PrismaClient({
+		 datasourceUrl: env.DATABASE_URL
+		}).$extends(withAccelerate())
+
+	const response=	await prisma.log.create({
+			data:{
+				level:'Info',
+				message:`${request.method} ${request.url}`,
+				meta:{
+					headers: JSON.stringify(request.headers),
+				},
+
+			},
+		})
+		
+		console.log(JSON.stringify(response))
+		return  Response.json(response)
 	},
-} satisfies ExportedHandler<Env>;
+}
